76b449c3f88eaddc0cf3a3db4cdfb89cd1ebf9ae
mpppk <niboshiporipori@gmail.com>
2023-08-31 01:07:36 +0900

    Implement heap

---
+++ pkgs/lib/src/algorithm/heap.test.ts
@@ -0,0 +1,28 @@
+import { describe, it, expect } from "vitest";
+import { Heap } from "./heap";
+
+describe("Heap", () => {
+  it("handle primitive number", () => {
+    const heap = new Heap<number>((v) => v);
+    heap.push(1);
+    heap.push(3);
+    heap.push(2);
+    expect(heap.pop()).toEqual({ data: 1, value: 1 });
+    expect(heap.pop()).toEqual({ data: 2, value: 2 });
+    expect(heap.pop()).toEqual({ data: 3, value: 3 });
+    expect(heap.pop()).toEqual({ data: null, value: null });
+    expect(heap.pop()).toEqual({ data: null, value: null });
+  });
+
+  it("handle object", () => {
+    const heap = new Heap<{ v: number }>((v) => v.v);
+    heap.push({ v: 1 });
+    heap.push({ v: 3 });
+    heap.push({ v: 2 });
+    expect(heap.pop()).toEqual({ data: { v: 1 }, value: 1 });
+    expect(heap.pop()).toEqual({ data: { v: 2 }, value: 2 });
+    expect(heap.pop()).toEqual({ data: { v: 3 }, value: 3 });
+    expect(heap.pop()).toEqual({ data: null, value: null });
+    expect(heap.pop()).toEqual({ data: null, value: null });
+  });
+});
